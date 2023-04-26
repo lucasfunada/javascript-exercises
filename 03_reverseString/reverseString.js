ef4b6fc435055edc624b37e957afd5c5d5fcab87
Lucas Funada <lucaskfunada@gmail.com>
2023-04-26 01:34:51 -0300

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    const array = str.split("");
+    let newStr = "";
+    for (let i = array.length - 1; i >= 0; i--) {
+        newStr += array[i];
+    }
+    return newStr;
 };
 
 // Do not edit below this line
