a30b8d04d05ace3fcf6141147e7c78646dab0a35
Lucas Funada <lucaskfunada@gmail.com>
2023-04-26 01:20:08 -0300

    Solve exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, repeats) {
+    if (repeats < 0) {
+        return "ERROR";
+    }
+    let newStr = "";
+    for(let i = 0; i < repeats; i++) {
+        newStr = newStr + str;
+    }
+    return newStr;
 };
 
 // Do not edit below this line
